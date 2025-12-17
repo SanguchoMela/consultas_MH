14c3db444d9cd423e38f593b4b30379494ca687c
Melany Sangucho <melany01-03@hotmail.com>
2025-12-17 11:11:27 -0500

    style: inputs, borders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -84,10 +84,11 @@ function App() {
         {/* <h1 className="text-3xl font-bold mb-4">Manta Hills</h1> */}
       </div>
 
+      {/* BUSCADOR */}
       <div className="grid md:grid-cols-2 grid-cols-1 gap-8 lg:m-7 m-5">
-        {/* Inputs para buscar clientes */}
-        <div className="flex flex-col space-y-1 min-w-[42%]">
-          <label className="label">Búsqueda por cliente</label>
+        {/* BUSCAR CLIENTE */}
+        <div className="card-search">
+          <h2 className="label text-cyan-700">🔍 Búsqueda por cliente</h2>
           <div className="flex flex-col">
             <label className="sub-label">Cédula</label>
             <input
@@ -110,15 +111,15 @@ function App() {
           </div>
           <button
             onClick={buscarClienteAuto}
-            className="bg-cyan-600 text-white font-medium mt-3 px-5 py-2 rounded hover:bg-cyan-700"
+            className="bg-cyan-600 text-white font-medium mt-3 px-5 py-2 rounded-lg hover:bg-cyan-700"
           >
             Buscar Cliente
           </button>
         </div>
 
-        {/* Inputs para buscar manzana y lote */}
-        <div className="flex flex-col space-y-1 min-w-[42%]">
-          <label className="label">Búsqueda por lote</label>
+        {/* BUSCAR LOTE */}
+        <div className="card-search">
+          <h2 className="label text-lime-700">🧱 Búsqueda por lote</h2>
           {/* <div className=""> */}
           <div className="flex flex-col ">
             <label className="sub-label">Lote</label>
@@ -143,7 +144,7 @@ function App() {
           {/* </div> */}
           <button
             onClick={buscarLote}
-            className="bg-lime-600 text-white font-medium mt-3 px-5 py-2 rounded hover:bg-lime-700"
+            className="bg-lime-600 text-white font-medium mt-3 px-5 py-2 rounded-lg hover:bg-lime-700"
           >
             Buscar Lote
           </button>
@@ -154,21 +155,29 @@ function App() {
       {error && <p className="text-red-600 mt-12 text-center">{error}</p>}
 
       {/* RESULTADOS */}
-      <section className="m-3 sm:m-4 md:m-5 lg:m-7">
+      <section className="m-3 sm:m-4 md:m-5 lg:m-7 space-y-3">
         {Array.isArray(resultados) && resultados.length > 0 ? (
           resultados.map((cliente) => (
-            <details key={cliente._id} className="lg:p-8 md:p-6 p-4 bg-white shadow rounded">
-              <summary className="cursor-pointer list-none flex justify-between items-center pb-2 border-b border-cyan-700/50">
+            <details key={cliente._id} className="group lg:p-6 md:p-5 p-3 rounded bg-white shadow shadow-cyan-900/70">
+              <summary className="cursor-pointer list-none flex justify-between items-center">
                 <h2 className="text-xl font-semibold text-center md:text-left">
                   {cliente.datosPersonales.nombrecliente}
                 </h2>
-                <span className="transition-transform group-open:rotate-180">                  
-                  <svg width="7vw" height="5vh" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z" fill="#005f78" />
-                  </svg>
-                </span>
+                <svg 
+                  className="w-7 h-7 transition-transform duration-500 group-open:rotate-180" 
+                  viewBox="0 0 24 24" 
+                  fill="none" 
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path 
+                    fill-rule="evenodd" 
+                    clip-rule="evenodd" 
+                    d="M12.7071 14.7071C12.3166 15.0976 11.6834 15.0976 11.2929 14.7071L6.29289 9.70711C5.90237 9.31658 5.90237 8.68342 6.29289 8.29289C6.68342 7.90237 7.31658 7.90237 7.70711 8.29289L12 12.5858L16.2929 8.29289C16.6834 7.90237 17.3166 7.90237 17.7071 8.29289C18.0976 8.68342 18.0976 9.31658 17.7071 9.70711L12.7071 14.7071Z" 
+                    fill="#005f78" 
+                  />
+                </svg>
               </summary>
 
+              <hr className="text-cyan-700 mt-2" />
               {/* Datos del cliente */}
               <table className="w-full lg:max-w-[50%]">
                 <tbody>
