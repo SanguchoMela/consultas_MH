b50611d9e6d192731e430efaae00bbcb4ca6d274
Melany Sangucho <melany01-03@hotmail.com>
2025-12-17 12:28:44 -0500

    fix: footer import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Footer from "./components/footer";
+import Footer from "./components/Footer"
 
 function App() {
   const [nombre, setNombre] = useState("");
