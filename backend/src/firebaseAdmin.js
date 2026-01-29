cdb4915b03523f8f1fe2f57e9b4833cf4a60552a
Melany Sangucho <melany01-03@hotmail.com>
2026-01-29 09:49:31 -0500

    fix: firebase config

--- backend/src/firebaseAdmin.js
+++ backend/src/firebaseAdmin.js
@@ -6,12 +6,23 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "serviceAccountKey.json"), "utf8")
-);
+let credential
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+  // Prod
+  credential = admin.credential.cert(
+    JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)
+  )
+} else {
+  // Local
+  const serviceAccount = JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, "serviceAccountKey.json"), "utf8"
+    )
+  )
+  credential = admin.credential.cert(serviceAccount)
+}
+
+admin.initializeApp({ credential});
 
 export default admin;
