8852a677e168453140d1987cc0b06c5c991520e5
Melany Sangucho <melany01-03@hotmail.com>
2026-01-30 12:39:13 -0500

    style: sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
     }
 
     const linkClass = (path) =>
-        `block py-2 px-4 rounded-lg hover:bg-cyan-900/30 ${location.pathname === path ? "font-semibold bg-cyan-900/30 text-white" : ""
+        `block py-2 px-4 mx-4 my-3 rounded-md hover:bg-cyan-900/60 hover:text-white ${location.pathname === path ? "font-semibold bg-cyan-900/60 text-white" : ""
         }`;
 
     useEffect(() => {
@@ -30,7 +30,7 @@ export default function Sidebar() {
             <div className={`
                 md:hidden fixed top-0 py-2 px-8 z-50 
                 flex justify-between items-center text-cyan-950 
-                ${open ? "bg-none" : "bg-[#b7c9d4]"}
+                ${open ? "bg-none" : "bg-[#d2dde3]"}
                 w-full backdrop-blur-sm`
             }>
                 <p className={`font-semibold text-lg transition-opacity duration-200 ${open ? "opacity-0" : "opacity-100"}`}>Manta Hills</p>
@@ -47,12 +47,13 @@ export default function Sidebar() {
                 <div onClick={() => setOpen(false)} className="fixed inset-0 bg-gray-400/40 z-40 md:hidden" />
             )}
 
+            {/* bg-[#b7c9d4] */}
             {/* Sidebar */}
             <aside
                 className={`
                 fixed md:static top-0 left-0 z-50 
                 min-h-full w-64 
-                bg-[#b7c9d4] font-medium text-gray-50 
+                bg-[#d2dde3] font-medium text-gray-900
                 rounded-r-2xl md:rounded-2xl
                 transform transition-transform duration-300
                 ${open ? "translate-x-0" : "-translate-x-full"}
@@ -63,24 +64,23 @@ export default function Sidebar() {
                 {/* Header */}
                 <div className="flex flex-col items-center py-4">
                     <img src="/mh.png" alt="Manta Hills Logo" className="w-32" />
-                    {/* <span className="text-sm font-medium">Manta Hills</span> */}
                 </div>
-                <div className="flex flex-col font-normal items-center py-2 border-b border-cyan-900/20">
+                <div className="flex flex-col text-sm items-center py-2 border-b border-cyan-900/30">
                     <p>{translateRole(role)}</p>
                 </div>
 
                 {/* Nav */}
-                <nav className="space-y-3 p-4">
-                    <Link to="/dashboard" className={linkClass("/dashboard")}>
-                        Dashboard
-                    </Link>
-
+                <nav>
                     <Link to="/lotes" className={linkClass("/lotes")}>
-                        Lotes
+                        Consulta de Clientes
                     </Link>
 
-                    <Link to="/docs" className={linkClass("/docs")}>
-                        Documentos
+                    <Link to="/documentos" className={linkClass("/documentos")}>
+                        Documentos Importantes
+                    </Link>
+
+                    <Link to="/manuales" className={linkClass("/manuales")}>
+                        Manuales de Procedimientos
                     </Link>
 
                     {role === "admin" && (
@@ -90,11 +90,12 @@ export default function Sidebar() {
                     )}
                 </nav>
 
+                {/* <hr className="text-cyan-900/30" /> */}
                 {/* Logout */}
-                <div className="w-full px-4">
+                <div className="w-full px-4 fixed bottom-0 mb-4">
                     <button
                         onClick={() => signOut(auth)}
-                        className="w-full bg-red-600 text-white font-medium py-2 rounded-lg hover:bg-red-700"
+                        className="w-full bg-red-500 text-white font-medium py-2 rounded-md hover:bg-red-600"
                     >
                         Cerrar sesión
                     </button>
