c588b1cc2156ec83712311cb4bc946dc1030920a
Melany Sangucho <melany01-03@hotmail.com>
2026-02-23 15:02:14 -0500

    feat: add docs

--- frontend/src/pages/Docs.jsx
+++ frontend/src/pages/Docs.jsx
@@ -20,13 +20,38 @@ export default function Docs() {
                             Ver documento
                         </a>
                     </li>
-
                     <li className="card-list">
                         <p className="md-label">
                             Plano I Etapa – Manta Hills
                         </p>
                         <a
                             href="https://drive.google.com/file/d/1Lq9EmONrGjuzsS4HUrZ6n3HEDLPN5_ti/view?usp=sharing"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="search-button"
+                        >
+                            Ver plano
+                        </a>
+                    </li>
+                    <li className="card-list">
+                        <p className="md-label">
+                            Plano II Etapa – Manta Hills
+                        </p>
+                        <a
+                            href="https://drive.google.com/file/d/1DXCyqUEnjCfQCaZgfr0KomTnlpFTT-z5/view?usp=sharing"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="search-button"
+                        >
+                            Ver plano
+                        </a>
+                    </li>
+                    <li className="card-list">
+                        <p className="md-label">
+                            Nomenclatura y Precios - II Etapa
+                        </p>
+                        <a
+                            href="https://drive.google.com/file/d/1xCWxG3jy4ktDZtzaFOoKIeacZeCGeGUj/view?usp=sharing"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="search-button"
