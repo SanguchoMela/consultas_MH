fb8d6fdd9f1c33f064fb2d8193d7dac030737440
Melany Sangucho <melany01-03@hotmail.com>
2026-08-13 11:12:42 -0500

    update: reference date in mora

--- backend/src/utils/mora.js
+++ backend/src/utils/mora.js
@@ -35,7 +35,7 @@ export const calcularDiasMora = (fecha) => {
   if (!f) return 0;
 
   // const hoy = new Date();
-  const hoy = new Date(2026, 5, 30);
+  const hoy = new Date(2026, 6, 31);
 
   // Ignorar la hora para evitar diferencias por huso horario
   const fechaInicio = new Date(f.getFullYear(), f.getMonth(), f.getDate());
